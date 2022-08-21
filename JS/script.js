d098551151c26c226411235e8406874e1a622a11
Samat <111216263+SamatSaliev@users.noreply.github.com>
2022-08-21 22:20:40 +0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+" use strict ";
+
+let weight = 'Samat';
+ console.log(weight);
+
+ const wall = "7";
+  console.log (wall);
+  var obj = 5 ;
+    console.log (obj);
+
+let district = 'gg';
+    console.log (district);
+ 
+    let geve = 'Load';
+    console.log(geve);
+
+
+    let number = 4;
+    console.log(-3*8);
+    console.log ('string'/3);
+
+    const bool = true;
+    console.log ('bool');
+
+    const object = {
+      name: 'Smith',
+      age: '17',
+      ismarried:false
+    };
+    console.log (object.ismarried);
+
+ let arr = [ 'orange.jpg' , 'apple.png' , 3 , 'water.pmb'];
+ console.log (arr[2]);
+  let gets = ['opinion','interest', 7 , 'game'];
+  console.log(gets[0]);
---
+++ JS/script2.js
@@ -0,0 +1,56 @@
+//alert('hello'); 
+//const result = confirm('Are you sure?');
+//console.log(result);
+// confirm - отмена ок
+
+//const answer = prompt("Вы уверены?", "");
+//console.log (answer);
+//prompt - вопрос,гле пользователь может написать ответ
+
+//const answers = +prompt("сколько вам лет?", "");
+//console.log (answers + 3);
+// const answ = [];
+
+//answ[0] = prompt("Как вас зовут?", "");
+//answ[1] = prompt("Какая у вас фамилия?", "");
+//answ[2] = prompt("сколько вам лет?", "");
+//console.log(typeof(answ));
+
+//const category = 'toys';
+ //console.log(`https://someurl.com/${category}/5`);
+
+ //const user = 'Самат';
+ //alert(`Привет,${user}`);
+
+ let incr = 10,
+     decr = 10;
+//incr++;
+//decr--;
+console.log(++incr);
+console.log(--decr);
+
+console.log(2*4 == 8);
+
+const isChecked = true,
+      isClose = false;
+//console.log(isChecked && isClose);
+console.log(isChecked && !isClose);
+
+//&& - и    || - или  !-меняет на true 
+
+const me = true,
+      you = false;
+console.log(me && !you);
+ 
+console.log(3+3*3 != 22);
+console.log("arr"+ '-object');
+console.log(4 + '-object');
+
+const werga = true ,
+      dirga = false;
+console.log(werga || dirga);    
+console.log(5+2*3 == 11);
+console.log("hello");
+
+
+
